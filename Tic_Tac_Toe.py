a67bee6f93241cd7ff533fd2d0ca4e586df665c3
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-29 19:38:01 +0530

    description added

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -5,7 +5,7 @@ Created on Mon Nov 29 21:05:40 2019
 @author: DHRUV
 """
 
-
+# assign 10 empty space for board
 board = [' ' for x in range(10)]
 
 def insertLetter(letter, pos):
@@ -29,6 +29,7 @@ def printBoard(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
+# predict the winner function
 def isWinner(bo, le):
     return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
 
